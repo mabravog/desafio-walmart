37de44e241f504ceb6d1d117fea7b84ae4a3eef6
Matías Bravo <mabravogalvez@gmail.com>
2020-08-04 23:52:10 -0400

    Router.

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-const index = fs.readFileSync('index.html');
+const router = require('./src/router');
+const server = http.createServer(router);
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end(index);
-}
-
-const server = http.createServer(requestListener);
 server.listen(process.env.PORT || 8080);
---
+++ src/router.js
@@ -0,0 +1,26 @@
+const url = require('url');
+const fs = require('fs');
+const index = fs.readFileSync('index.html');
+
+function router(req, res) {
+    const reqUrl = url.parse(req.url, true);
+
+    switch (reqUrl.pathname) {
+        case '/':
+            res.writeHead(200);
+            res.end(index);
+            break;
+
+        case '/products':
+            res.writeHead(200);
+            res.end({});
+            break;
+    
+        default:
+            res.writeHead(500);
+            res.end('Error');
+            break;
+    }
+}
+
+export default router;
